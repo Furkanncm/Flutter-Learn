a63b825bac16e078c18bd1cede3fec79b237f02d
Furkan Kazim Cam <furkankazimca@gmail.com>
2024-05-14 14:39:11 +0300

    chore: Add pageView widget for navigating between pages

---
+++ furkann/lib/101/pageView_learn.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:furkann/101/firstPage.dart';
+import 'package:furkann/101/listTile.dart';
+import 'package:furkann/101/mainFood.dart';
+import 'package:furkann/101/randomPage.dart';
+
+class pageView extends StatefulWidget {
+  const pageView({super.key});
+
+  @override
+  State<pageView> createState() => _pageViewState();
+}
+
+class _pageViewState extends State<pageView> {
+  final _pageController = PageController();
+
+  int currentPage = 1;
+  void _pageCounter(int index) {
+    setState(() {
+      currentPage = index + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 58),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _floatingAcButton(
+              icon: Icon(Icons.chevron_left),
+              method: () {
+                _pageController.previousPage(
+                    duration: customDuration.durationTime,
+                    curve: _customCurve.CustomCurve);
+              },
+            ),
+            Text(currentPage.toString()),
+            _floatingAcButton(
+              icon: Icon(Icons.chevron_right),
+              method: () {
+                _pageController.nextPage(
+                    duration: customDuration.durationTime,
+                    curve: _customCurve.CustomCurve);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: _pageCounter,
+        children: [
+          firstPage(),
+          listTile(),
+          mainFood(),
+        ],
+      ),
+    );
+  }
+}
+
+class _floatingAcButton extends StatelessWidget {
+  final Icon icon;
+  final VoidCallback method;
+  const _floatingAcButton({
+    Key? key,
+    required this.icon,
+    required this.method,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: method,
+      child: icon,
+    );
+  }
+}
+
+class customDuration {
+  static final durationTime = Duration(seconds: 1);
+}
+
+class _customCurve {
+  static final CustomCurve = Curves.easeInOut;
+}
