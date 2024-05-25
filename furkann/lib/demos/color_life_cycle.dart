4b1dd5cf9a89897e83c7a008f747aa45bfabb913
Furkanncm <dev.furkankazimcam@gmail.com>
2024-05-25 15:07:50 +0300

    commit deneme

--- furkann/lib/demos/color_life_cycle.dart
+++ furkann/lib/demos/color_life_cycle.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class colorLifeCycle extends StatefulWidget {
   const colorLifeCycle({super.key});
+//deneme
 
   @override
   State<colorLifeCycle> createState() => _colorLifeCycleState();
@@ -10,6 +11,8 @@ class colorLifeCycle extends StatefulWidget {
 class _colorLifeCycleState extends State<colorLifeCycle> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
