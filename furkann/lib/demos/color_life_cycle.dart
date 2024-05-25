78101c1a3c0fa2003237baefa79f2950be0e5b65
Furkanncm <dev.furkankazimcam@gmail.com>
2024-05-25 15:05:12 +0300

    feat: Add color life cycle demo page

---
+++ furkann/lib/demos/color_life_cycle.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class colorLifeCycle extends StatefulWidget {
+  const colorLifeCycle({super.key});
+
+  @override
+  State<colorLifeCycle> createState() => _colorLifeCycleState();
+}
+
+class _colorLifeCycleState extends State<colorLifeCycle> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
